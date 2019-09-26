942f5c427e36fce63c1053d79ef406615e54c900
Amir <amirasyraf@outlook.com>
2019-09-26 17:47:25 +0800

    admin sidebar update

--- resources/views/partials/admin_sidebar.blade.php
+++ resources/views/partials/admin_sidebar.blade.php
@@ -16,15 +16,15 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown ">
-                        <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon ti ti-credit-card"></i>Bank</a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"  aria-expanded="false"> <i class="menu-icon ti ti-credit-card"></i>Bank</a>
                         <ul class="sub-menu children dropdown-menu ">
                             <li class="{{ Route::currentRouteNamed('admin-assign-bank') ? 'active': '' }}">
-                                <a href="{{url('admin_assign_bank')}}"><i class="menu-icon ti ti-credit-card">
+                                <a href="{{url('admin_assign_bank')}}"><i class="menu-icon">
                                 </i>{{ trans('sidebar.bank_assign') }} </a>
                             </li>
 
                             <li class="{{ Route::currentRouteNamed('admin-bank-list') ? 'active': '' }}">
-                                <a href="User_Search.html"><i class="menu-icon ti ti-credit-card">
+                                <a href="User_Search.html"><i class="menu-icon">
                                 </i>{{ trans('sidebar.bank_details') }} </a>
                             </li>
                         </ul>
