ff7087e0bb841310c960697c052722ef65a83858
Amir <amirasyraf@outlook.com>
2019-10-01 14:04:41 +0800

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,18 @@
 <footer class="site-footer" style="position:fixed; width:100%; bottom:0" >
     <div class="footer-inner bg-white">
         <div class="row">
-            <div class="col-sm-1">
-                <a href="http://www.modularsoft.com.my/#about">{{ trans('footer.about')}}</a>
-            </div>
-            <div class="col-sm-8">
-                    <a href="http://www.modularsoft.com.my/#contact">{{ trans('footer.contact') }}</a>
+            <div class="col-sm-8 float-left">
+                <div>
+                <a href="http://www.modularsoft.com.my/#about" style="padding-right:40px">{{ trans('footer.about')}}</a>
+                <a href="http://www.modularsoft.com.my/#contact">{{ trans('footer.contact') }}</a>
                 </div>
-            <div class="col-sm-3 text-right">
+            </div>
+            <div class="col-sm-4 float-right">
+                <div>
                 &copy;<a href="http://www.modularsoft.com.my/">Modularsoft Sdn Bhd</a>
+                </div>
             </div>
         </div>
+        
     </div>
 </footer>
