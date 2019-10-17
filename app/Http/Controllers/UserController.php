7576568dd3a4603b7f743fbdb30be51fbeccaab1
naanii <syaazmz@gmail.com>
2019-10-17 10:08:33 +0800

    changePassword

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use App\Bank;
 use App\ClientDetail;
 use Auth;
 use DB;
+use \Hash;
 
 class UserController extends Controller
 {
@@ -17,6 +18,9 @@ class UserController extends Controller
      * User and admin login page
      * User consist of normal user and reviewer
      */
+
+    
+
     public function loginUser()
     {
     	return view('auth.login_user');
@@ -30,6 +34,29 @@ class UserController extends Controller
     {
         return view('users.forgot_password');
     }
+
+    public function changePassword(Request $request){
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:8|confirmed',
+        ]);
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+        return redirect()->back()->with("success","Password changed successfully !"); 
+    }
+
+    
+
 
     /**
      * normal user and reviewer landing page
